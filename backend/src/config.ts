859245c40b34995401d83b54092cbeadadeb6eab
Julietrantt <1995julietran@gmail.com>
2022-09-22 15:02:52 +0200

    setting path for env and correct DATABASE

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,6 +1,6 @@
 import { config } from 'dotenv';
 
-config();
+config({path: "../.env"});
 
 export default {
   port: process.env.SERVER_PORT,
@@ -8,7 +8,7 @@ export default {
     host: process.env.DB_HOST,
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABSE,
+    database: process.env.DB_DATABASE,
     port: process.env.DB_PORT,
   },
 };
