3c21c6f1ff2b75be4a1b8afdda18684f511a48f7
Julietrantt <1995julietran@gmail.com>
2022-10-04 13:18:21 +0200

    remove workstation_isactive so it will be not validate

--- backend/src/service/todo/schema.ts
+++ backend/src/service/todo/schema.ts
@@ -3,7 +3,6 @@ import Joi from 'joi';
 export const workstationSchema = Joi.object().keys({
   floor_id: Joi.number().required(),
   workstation_name: Joi.string().required(),
-  workstation_isactive: Joi.boolean(),
 });
 
 export const seatSchema = Joi.object().keys({
