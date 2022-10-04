65822ea7048eb8256f33d5ef9a0ff92f9d1fc49b
Károly András Kovács <karoly.kovacs@greenfoxacademy.com>
2022-10-04 22:57:19 +0200

    hotfix(backend/service/building): resolve merge conflict

--- backend/src/service/building/buildingService.ts
+++ backend/src/service/building/buildingService.ts
@@ -18,15 +18,10 @@ export class BuildingService {
     return this.buildingRepository.listBuildings();
   }
 
-<<<<<<< HEAD
   async getSingleBuilding(id: number): Promise<BuildingEntity | []> {
     const findBuilding = await this.buildingRepository.getSingleBuilding(id);
     if (findBuilding === null) return [];
     return findBuilding;
-=======
-  getSingleBuilding(buildingId: number): Promise<BuildingEntity | null> {
-    return this.buildingRepository.getSingleBuilding(buildingId);
->>>>>>> 1dfeff52448f42b5296542debc5281eb3b077aaf
   }
 
   async addNewBuilding(requestBody: BuildingEntity): Promise<Success> {
@@ -77,7 +72,6 @@ export class BuildingService {
     return { success: 'yes' };
   }
 
-<<<<<<< HEAD
   async deleteBuilding(id: number): Promise<Success> {
     try {
       const requestDataValidation = await idSchema.validateAsync(id);
@@ -90,9 +84,5 @@ export class BuildingService {
 
     const deleteABuilding = await this.buildingRepository.deleteBuilding(id);
     return { success: 'yes' };
-=======
-  deleteBuilding(id: number): Promise<Success> {
-    return this.buildingRepository.deleteBuilding(id);
->>>>>>> 1dfeff52448f42b5296542debc5281eb3b077aaf
   }
 }
