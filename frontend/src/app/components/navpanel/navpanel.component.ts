086b8edbb4964340cfef5f31881343eb9ced0e65
Károly András Kovács <karoly.kovacs@greenfoxacademy.com>
2022-10-06 14:54:59 +0200

    fix(frontend/navpanel): remove unused ngoninit

--- frontend/src/app/components/navpanel/navpanel.component.ts
+++ frontend/src/app/components/navpanel/navpanel.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'navpanel',
@@ -7,6 +7,4 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavpanelComponent {
   constructor() {}
-
-  ngOnInit(): void {}
 }
