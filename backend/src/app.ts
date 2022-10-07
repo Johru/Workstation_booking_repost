179d04dabd89c394d749326b7b7b1443c4f869d9
Tomas Max <tomasmax92@gmail.com>
2022-10-07 16:01:58 +0200

    feat(PFEWB-61): added allowed domains that can access our api through CORS

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -7,10 +7,24 @@ import { userRouter } from './route/userRoute';
 import { authRouter } from './route/userRoute';
 import cors from 'cors';
 
+const allowedDomains = ['http://localhost:4200/'];
+
 const app = express();
 
 app.use(requestLogger);
-app.use(cors());
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // bypass the requests with no origin (like curl requests, mobile apps, etc )
+      if (!origin) return callback(null, true);
+      if (allowedDomains.indexOf(origin) === -1) {
+        var msg = `This site ${origin} does not have an access. Only specific domains are allowed to access it.`;
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
 
 app.use(express.json());
 app.use(reservationRouter);
