1d7b8113ea9a4b6dfa61cb8d1da1d24773473bf7
sinttax <martin.daru@gmail.com>
2022-10-11 11:50:25 +0200

    added methods: VerfiyJWT, isAdmin, isBlocked

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,8 +1,9 @@
 import { config } from 'dotenv';
+import path from 'path';
 const dirpath = require('path');
 const envpath = dirpath.join(__dirname, '../.env');
 
-config();
+config({ path: '../.env' });
 
 export default {
   port: process.env.SERVER_PORT,
