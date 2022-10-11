58b702029804b727c224155bbedc3f667f44df6e
Tomas Max <tomasmax92@gmail.com>
2022-10-11 14:32:57 +0200

    feat(PFEWB-58):added signOut on admin nav panel and prepared reroutes, waiting for the building frontend merge

--- frontend/src/app/components/admin-nav-panel/admin-nav-panel.component.ts
+++ frontend/src/app/components/admin-nav-panel/admin-nav-panel.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'admin-nav-panel',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./admin-nav-panel.component.css'],
 })
 export class AdminNavPanelComponent {
-  constructor() {}
+  constructor(private router: Router) {}
+
+  signOut() {
+    localStorage.removeItem('token');
+    this.router.navigate(['']);
+  }
 }
