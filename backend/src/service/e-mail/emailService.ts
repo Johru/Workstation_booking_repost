563dd23500dc391a3b841ccbc85843807bbffd76
Tomas Max <tomasmax92@gmail.com>
2022-10-12 14:53:28 +0200

    feat(PFEWB-71):after successfull registration user will receive welcome email

--- backend/src/service/e-mail/emailService.ts
+++ backend/src/service/e-mail/emailService.ts
@@ -1,21 +1,36 @@
-import { Client } from '@sendgrid/client';
-import { EmailData } from '@sendgrid/helpers/classes/email-address';
 import sgMail = require('@sendgrid/mail');
+import { UserEntity } from 'db';
 import { config } from 'dotenv';
-import { toNamespacedPath } from 'path';
-const dirpath = require('path');
-const envpath = dirpath.join(__dirname, '../.env');
+import logger from '../../logger';
 
 config();
 
-sgMail.setClient(new Client());
-
 sgMail.setApiKey(process.env.SENDGRID_API_KEY!);
-
 sgMail.setSubstitutionWrappers('{{', '}}');
 
 const mail = process.env.MAIL!;
 
 export class EmailService {
-  consructor() {}
+  constructor() {}
+
+  sendMail(user: UserEntity) {
+    const msg = {
+      to: user.user_email,
+      from: mail,
+      templateId: 'd-f19a565bef2447e68b3d43ca33a0e2f3',
+      dynamicTemplateData: {
+        name: user.user_name,
+        login: user.user_login,
+      },
+    };
+
+    sgMail
+      .send(msg)
+      .then(() => {
+        logger.info(`Email to ${user.user_email} sent.`);
+      })
+      .catch((error: Error) => {
+        logger.error(error);
+      });
+  }
 }
