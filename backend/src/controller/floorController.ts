6be5e247d101ea4d0352cc6996071b8cd1380a59
Julietrantt <1995julietran@gmail.com>
2022-10-20 11:38:37 +0200

    put verifyJWT to floorController

--- backend/src/controller/floorController.ts
+++ backend/src/controller/floorController.ts
@@ -1,16 +1,20 @@
 import { FloorEntity } from 'db';
 import { IFloorService } from '../service';
 import { Router, Response, Request } from 'express';
+import { AuthMiddleware } from 'middlewares';
 
 export class FloorController {
   private readonly _router: Router = Router();
 
-  constructor(private floorService: IFloorService) {
-    this._router.get('/floor/showall', async (req: Request, res: Response) => {
+  constructor(private floorService: IFloorService,
+    private authMiddleware: AuthMiddleware) {
+    const verifyJWT = this.authMiddleware.verifyJWT;
+
+    this._router.get('/floor/showall',verifyJWT, async (req: Request, res: Response) => {
       res.status(200).json(await this.floorService.getFloors());
     });
 
-    this._router.get('/building-floor', async (req: Request, res: Response) => {
+    this._router.get('/building-floor',verifyJWT, async (req: Request, res: Response) => {
       const buildingIdQuery = req.query.buildingId;
       if (buildingIdQuery === undefined) {
         res.status(400).json({
@@ -24,7 +28,7 @@ export class FloorController {
       }
     });
 
-    this._router.post('/floor/create', async (req: Request, res: Response) => {
+    this._router.post('/floor/create',verifyJWT, async (req: Request, res: Response) => {
       const buildingIdQuery = req.query.buildingId;
       if (buildingIdQuery === undefined) {
         res.status(400).json({
@@ -40,7 +44,7 @@ export class FloorController {
     });
 
     this._router.put(
-      '/floor/:floorId/update',
+      '/floor/:floorId/update',verifyJWT,
       async (req: Request, res: Response) => {
         const floor: FloorEntity = req.body as FloorEntity;
         const floorId = parseInt(req.params.floorId, 10);
@@ -51,7 +55,7 @@ export class FloorController {
     );
 
     this._router.delete(
-      '/floor/:floorId/delete',
+      '/floor/:floorId/delete',verifyJWT,
       async (req: Request, res: Response) => {
         const floorId = parseInt(req.params.floorId, 10);
         res.status(200).json(await this.floorService.deleteFloor(floorId));
