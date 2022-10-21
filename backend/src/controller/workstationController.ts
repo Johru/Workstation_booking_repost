3b921b523359b59a472721a65967816ab134ae60
Julietrantt <1995julietran@gmail.com>
2022-10-21 09:02:48 +0200

    changes in Route

--- backend/src/controller/workstationController.ts
+++ backend/src/controller/workstationController.ts
@@ -1,7 +1,7 @@
 import { Router, Response, Request } from 'express';
 import { IWorkstationService } from '../service';
 import { WorkstationEntity } from '../db';
-import { AuthMiddleware } from 'middlewares';
+import { AuthMiddleware } from '../middlewares';
 
 export class WorkstationController {
   private readonly _router: Router = Router();
